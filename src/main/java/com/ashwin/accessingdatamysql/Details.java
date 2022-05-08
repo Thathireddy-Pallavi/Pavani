d7146a9f97b7160dc732f8b29de44713c438a231
Thathireddy-Pallavi <104891621+Thathireddy-Pallavi@users.noreply.github.com>
2022-05-08 10:59:39 +0530

    Update Details.java

--- src/main/java/com/ashwin/accessingdatamysql/Details.java
+++ src/main/java/com/ashwin/accessingdatamysql/Details.java
@@ -2,7 +2,6 @@ package com.ashwin.accessingdatamysql;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity // This tells Hibernate to make a table out of this class
