0312143753b0e21700f0baf0757af07dd24ab8b1
Thathireddy-Pallavi <104891621+Thathireddy-Pallavi@users.noreply.github.com>
2022-05-08 10:56:22 +0530

    Update GreetingController.java

--- src/main/java/com/ashwin/controller/GreetingController.java
+++ src/main/java/com/ashwin/controller/GreetingController.java
@@ -1,6 +1,5 @@
 package com.ashwin.controller;
 
-import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 import com.ashwin.accessingdatamysql.Details;
 import com.ashwin.accessingdatamysql.DetailsRepository;
